2488ae5e3d7223d87b3e92e882676a7dc4217677
DanielaIH <4427@holbertonschool.com>
2023-03-22 22:13:58 -0500

    ADD math/linear_algebra/2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import numpy as np
+"""function that calculates the shape of a matrix"""
+def matrix_shape(matrix):
+    """shape of a matrix"""
+    return(np.array(np.shape(matrix)))
