5312e0f285c2b8f19a0efccd97aa7bcaa056f6e1
DanielaIH <4427@holbertonschool.com>
2023-03-23 00:14:07 -0500

    ADD math/linear_algebra/7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,15 +6,15 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices"""
     new_matrix = []
     try:
-        if (axis):
+        if (len(mat1[0]) == len(mat2[0])):
+            if (axis):
+                new_matrix = [row[:] for row in mat1]
+                for i in range(len(new_matrix[0])):
+                    new_matrix[i].append(mat2[i][0])
+                return(new_matrix)
             new_matrix = [row[:] for row in mat1]
-            for i in range(len(new_matrix[0])):
-                new_matrix[i].append(mat2[i][0])
+            for i in range(len(mat2)):
+                new_matrix.append(mat2[i])
             return(new_matrix)
-        new_matrix = [row[:] for row in mat1]
-
-        for i in range(len(mat2)):
-            new_matrix.append(mat2[i])
-        return(new_matrix)
     except IndexError:
         return None
