9f33ce540140c06f0b78425ac661151726695de7
DanielaIH <4427@holbertonschool.com>
2023-03-23 21:30:49 -0500

    ADD math/linear_algebra/7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -19,12 +19,14 @@ def cat_matrices2D(mat1, mat2, axis=0):
                 return None
             try:
                 if (len(mat1[i]) == 0):
-                    return None
+                    if len(mat2[i]) > 0:
+                        return None
             except IndexError:
                 pass
             try:
                 if (len(mat2[i]) == 0):
-                    return None
+                    if len(mat1[i]) > 0:
+                        return None
             except IndexError:
                 pass
             new_matrix.append(mat2[i])
