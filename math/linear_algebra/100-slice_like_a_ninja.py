7bfee57a8f55d6d476d8344e96299cee4504a275
DanielaIH <4427@holbertonschool.com>
2023-03-23 23:06:52 -0500

    ADD 100-slice_like_a_ninja.py

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function that slices a matrix along specific axes"""
+
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """slices a matrix"""
+    new_matrix = (max(axes) + 1) * [slice(None)]
+    for k, v in axes.items():
+        new_matrix[k] = slice(*v)
+    result = matrix[tuple(new_matrix)]
+    return result
