f1c73bed426f24e32a849c4a0f6c6b7845fd5663
DanielaIH <4427@holbertonschool.com>
2023-03-23 20:54:24 -0500

    ADD math/linear_algebra/7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -23,32 +23,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return(new_matrix)
     except IndexError:
         return None
-
-m1 = [[1]]
-m2 = [[2]]
-m = cat_matrices2D(m1, m2)
-if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
-    print("Not a new matrix")
-
-print(m)
-m1 = [[4, -7, 56, 2], [5, 106, 7, 2]]
-m2 = [[2, -6, 3, 23], [0, -6, 3, 42], [73, 8, 2, 99]]
-m = cat_matrices2D(m1, m2)
-if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
-    print("Not a new matrix")
-
-print(m)
-m1 = [[484, 247, -556], [554, 16, 75], [5, 88, 23]]
-m2 = [[233, -644, 325], [406, -16, 33]]
-m = cat_matrices2D(m1, m2, axis=0)
-if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
-    print("Not a new matrix")
-
-print(m)
-m1 = [[-54, -87, 56, -92, 81], [54, 16, -72, 42, 901]]
-m2 = [[12, 63], [-10, 69]]
-m = cat_matrices2D(m1, m2, axis=1)
-if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
-    print("Not a new matrix")
-
-print(m)
