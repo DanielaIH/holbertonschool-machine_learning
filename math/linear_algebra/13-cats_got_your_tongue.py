67aca987ad1fff951b6dfe10313a0e9fa1aceea7
DanielaIH <4427@holbertonschool.com>
2023-03-23 22:42:48 -0500

    ADD math/linear_algebra/13-cats_got_your_tongue.py

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+"""function that concatenates two matrices along a specific axis"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """concatenates two matrices"""
+    return(np.append(mat1, mat2, axis=axis))
