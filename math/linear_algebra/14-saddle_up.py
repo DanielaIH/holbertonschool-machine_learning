6dcb772751c99d58d33334aa6e7a0203039f5e6b
DanielaIH <4427@holbertonschool.com>
2023-03-23 22:53:42 -0500

    ADD 14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function that performs matrix multiplications"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """performs matrix multiplication"""
+    return(np.matmul(mat1, mat2))
