b85fcfcf14bb612a5f49e1ca23b29378f2f2b8cd
DanielaIH <4427@holbertonschool.com>
2023-03-23 22:37:15 -0500

    ADD math/linear_algebra/12-bracin_the_elements.py

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""that performs element-wise addition, subtraction,
+multiplication, and division:"""
+
+
+def np_elementwise(mat1, mat2):
+    """transposes matrix"""
+    return(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
