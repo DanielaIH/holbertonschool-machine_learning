4d53c6010f5390869f790270ccb44229fa958274
DanielaIH <4427@holbertonschool.com>
2023-03-23 20:52:52 -0500

    ADD math/linear_algebra/7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,15 +6,49 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices"""
     new_matrix = []
     try:
-        if (len(mat1[0]) == len(mat2[0])):
-            if (axis):
-                new_matrix = [row[:] for row in mat1]
-                for i in range(len(new_matrix[0])):
-                    new_matrix[i].append(mat2[i][0])
-                return(new_matrix)
+        if (axis):
             new_matrix = [row[:] for row in mat1]
-            for i in range(len(mat2)):
-                new_matrix.append(mat2[i])
+            for i in range(len(mat1)):
+                new_matrix[i] += mat2[i]
             return(new_matrix)
+        new_matrix = [row[:] for row in mat1]
+        for i in range(len(mat2)):
+            try:
+                if(mat1[i] and mat2[i]):
+                    new_matrix.append(mat2[i])
+                else:
+                    return None
+            except:
+                pass
+        return(new_matrix)
     except IndexError:
         return None
+
+m1 = [[1]]
+m2 = [[2]]
+m = cat_matrices2D(m1, m2)
+if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
+    print("Not a new matrix")
+
+print(m)
+m1 = [[4, -7, 56, 2], [5, 106, 7, 2]]
+m2 = [[2, -6, 3, 23], [0, -6, 3, 42], [73, 8, 2, 99]]
+m = cat_matrices2D(m1, m2)
+if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
+    print("Not a new matrix")
+
+print(m)
+m1 = [[484, 247, -556], [554, 16, 75], [5, 88, 23]]
+m2 = [[233, -644, 325], [406, -16, 33]]
+m = cat_matrices2D(m1, m2, axis=0)
+if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
+    print("Not a new matrix")
+
+print(m)
+m1 = [[-54, -87, 56, -92, 81], [54, 16, -72, 42, 901]]
+m2 = [[12, 63], [-10, 69]]
+m = cat_matrices2D(m1, m2, axis=1)
+if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
+    print("Not a new matrix")
+
+print(m)
