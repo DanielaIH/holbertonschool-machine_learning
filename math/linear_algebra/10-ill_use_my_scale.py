5541dc49204aab91799da69228878a16f753dff5
DanielaIH <4427@holbertonschool.com>
2023-03-23 22:21:49 -0500

    ADD /math/linear_algebra/10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+"""function  that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """shape of a numpy.ndarray"""
+    return(matrix.shape)
