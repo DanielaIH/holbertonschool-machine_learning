f62f13c06b4bad6ac66eb0a60769f6dee9e83323
DanielaIH <4427@holbertonschool.com>
2023-03-31 22:42:03 -0500

    ADD task

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,4 +6,4 @@ def poly_derivative(poly):
     """derivative of a polynomial"""
     if type(poly) is not list or len(poly) <= 0:
         return None
-    return [poly[i] * i for i in range(1, len(poly))]
+    return [poly[i] * i for i in range(poly)]
