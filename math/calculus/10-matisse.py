32803fa420bf476c109339463ddb7813da8ea11d
DanielaIH <4427@holbertonschool.com>
2023-03-31 22:40:08 -0500

    ADD task

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function that calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """derivative of a polynomial"""
+    if type(poly) is not list or len(poly) <= 0:
+        return None
+    return [poly[i] * i for i in range(1, len(poly))]
