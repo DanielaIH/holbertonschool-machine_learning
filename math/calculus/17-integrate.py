e91b1dd9f7b205042b867fbea3c93206aecad987
DanielaIH <4427@holbertonschool.com>
2023-03-31 23:21:33 -0500

    ADD task

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -7,5 +7,7 @@ def poly_integral(poly, C=0):
     if type(poly) is not list or len(poly) <= 0 or type(C) is not int:
         return None
     if len(poly) == 1:
-        return [0]
-    return [C] + [(poly[i] / (i + 1)) if (poly[i] / (i + 1)) % 1 != 0 else int((poly[i] / (i + 1))) for i in range(len(poly))]
+        return [C]
+    
+    x = lambda i:i if i % 1 != 0 else i
+    return [C] + [x(poly[i] / (i + 1)) for i in range(len(poly))]
