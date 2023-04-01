31fd1f513bd6d2fb1a6bc37fb4ef031d28ee0cc8
DanielaIH <4427@holbertonschool.com>
2023-03-31 22:17:05 -0500

    ADD task

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,13 @@
 
 def summation_i_squared(n):
     """summation i squared"""
+    if not n:
+        return None
     if n <= 1:
-        return (n ** 2)
+        return n
     else:
         return (n ** 2) + summation_i_squared(n - 1)
+
+
+a = summation_i_squared(5)
+print(a)
