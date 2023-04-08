3e8e79296d30a6f2fe8a23a4ff9163ed4675550c
DanielaIH <4427@holbertonschool.com>
2023-04-07 21:34:39 -0500

    ADD normal.py

---
+++ math/probability/normal.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import numpy as np
+"""class Normal that represents a normal distribution"""
+
+
+class Normal:
+    """class Normal"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """class Normal __init__"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.stddev = float(stddev)
+            self.mean = float(mean)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = np.sqrt(sum((x - self.mean) ** 2 for x in data) / (len(data) - 1))
+
+    def pdf(self, x):
+        """lambda e^{{-lambda x}}"""
+        if x < 0:
+            return 0
+        else:
+            return self.lambtha * (self.e ** (- self.lambtha * x))
+
+    def cdf(self, x):
+        """{ 1-e^{-lambda x}}"""
+        if x < 0:
+            return 0
+        else:
+            return 1 - (self.e ** (- self.lambtha * x))
