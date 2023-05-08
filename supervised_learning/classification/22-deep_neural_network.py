d574d73063fdf438887a3f225e68c9019f2cba37
DanielaIH <4427@holbertonschool.com>
2023-05-07 23:22:40 -0500

    ADD 22-deep_neural_network.py

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -73,6 +73,7 @@ class DeepNeuralNetwork:
             self.__weights['b'+str(l)] -= alpha * db
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Trains the deep neural network"""
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
