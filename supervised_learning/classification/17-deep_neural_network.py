57231e46bd9b34459dc15e5f6939f7d7d465bfd0
DanielaIH <4427@holbertonschool.com>
2023-05-07 21:30:31 -0500

    ADD 17-deep_neural_network.py

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Deep Neural Network"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """class NeuralNetwork that defines a deep neural
+    network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Initialize the Deep Neural Network"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.nx = nx
+        self.layers = layers
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        """inicializar los pesos y sesgos de la red con He et aL"""
+        for la in range(0, self.L):
+            if layers[la] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            if la == 0:
+                l_prev = nx
+            else:
+                l_prev = layers[la - 1]
+            self.weights['W' + str(la + 1)] = np.random.randn(
+                layers[la], l_prev) * np.sqrt(2 / l_prev)
+            self.weights['b' + str(la + 1)] = np.zeros((layers[la], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
