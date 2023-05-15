c508201a0a985518f245f009e89b665737e4cf46
DanielaIH <4427@holbertonschool.com>
2023-05-14 23:07:35 -0500

    ADD 1-input.py

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -15,7 +15,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i in range(len(layers)):
         x = K.layers.Dense(layers[i], activation=activations[i],
                            kernel_regularizer=k_reg)(x)
-        x = K.layers.Dropout(1 - keep_prob)(x)
+        if (i < len(layers) - 1):
+            x = K.layers.Dropout(1 - keep_prob)(x)
 
     model = K.Model(inputs=inputs, outputs=x)
     return model
