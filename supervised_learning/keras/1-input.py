69d9ce6037a488c58de5de2fc66bcbbba56788c4
DanielaIH <4427@holbertonschool.com>
2023-05-14 22:48:14 -0500

    ADD 1-input.py

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""supervised_learning keras"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """function that builds a neural network with the Keras library"""
+    if len(layers) != len(activations):
+        raise ValueError("Number of layers and activations must be the same")
+
+    k_reg = K.regularizers.l2(lambtha)
+    inputs = K.Input(shape=(nx,))
+    x = inputs
+
+    for i in range(len(layers)):
+        x = K.layers.Dense(layers[i], activation=activations[i],
+                           kernel_regularizer=k_reg)(x)
+        x = K.layers.Dropout(1 - keep_prob)(x)
+
+    model = K.Model(inputs=inputs, outputs=x)
+    return model
